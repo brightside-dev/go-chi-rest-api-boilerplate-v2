736058d4f804196fa4b6f3cd9b7fab8442c2ec03
Brightside <battousai.dev@proton.me>
2025-01-26 21:03:55 +1100

    implement cobra into the project

---
+++ cmd/command/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/brightside-dev/go-chi-rest-api-boilerplate-v2/cmd/command/cmd"
+
+func main() {
+	cmd.Execute()
+}
