7785c0ef53706c3b819da86198eed8731b9b8cb0
Brightside <battousai.dev@proton.me>
2025-02-01 19:37:51 +1100

    push wip

--- internal/push/push.go
+++ internal/push/push.go
@@ -1,1 +1,16 @@
 package push
+
+type Push struct {
+}
+
+func NewPush() *Push {
+	return &Push{}
+}
+
+func (p *Push) PushIOS() {
+	// do something
+}
+
+func (p *Push) PushAndroid() {
+	// do something
+}
